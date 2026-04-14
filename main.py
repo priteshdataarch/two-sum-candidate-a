b376f72be438230d42f723ae2496fbad360b44f1
Atul <atul.tamgave@mursion.com>
2026-04-14 10:32:24 +0530

    bruteforce

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-# main.py
-from clone_repo import clone_repo
-from analyzer import extract_structure
-from evaluator import evaluate_code
+def two_sum(nums, target):
+    result = []
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                result.append(nums[i])
+                result.append(nums[j])
+                return result
 
-def run(repo_url, problem_statement):
-    print("Cloning repo...")
-    repo_path = clone_repo(repo_url)
+nums = [2,7,11,15]
+target = 9
 
-    print("Analyzing structure...")
-    structure = extract_structure(repo_path)
-
-    print("Evaluating with LLM...")
-    result = evaluate_code(problem_statement, structure)
-
-    print("\n===== FINAL RESULT =====\n")
-    print(result)
-
-
-if __name__ == "__main__":
-    repo_url = input("Enter GitHub repo URL: ")
-    problem = input("Enter problem statement: ")
-
-    run(repo_url, problem)
+print(two_sum(nums, target))
